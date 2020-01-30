7bb856bdef669bddefd2d8076e68148e03943e67
donunobunjo <don.unobunjo@gmail.com>
2020-01-30 16:08:12 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const router = express.Router();
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+// Get authentication
+const credentials = {
+  apiKey: process.env.apiKey, // use your sandbox app API key for development in the test environment
+  //username: process.env.username // use 'sandbox' for development in the test environment
+  username:"Donoe"
+};
+
+const AfricasTalking = require("africastalking")(credentials);
+
+const sms = AfricasTalking.SMS;
+
+
+
+console.log(credentials);
+
+
+
+// Send SMS route
+router.post("/", (req, res) => {
+  const { to, message } = req.body || res.status(400).json({error: "Both 'to' and 'message' are required"});
+  sms
+    .send({ to, message, enque: true })
+    .then(response => {
+      console.log(response);
+      res.json(response);
+    })
+    .catch(error => {
+      console.log(error);
+      res.json(error.toString());
+    });
+});
+
+module.exports = router;
